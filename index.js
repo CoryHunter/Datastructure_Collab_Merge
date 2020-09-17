520e6f7756637d5fad726138d4a4b3d6522c01c0
Cory Hunter <coryhunt@byu.edu>
2020-09-16 21:41:35 -0600

    GOJS Infastructure

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function init() {
-    var $ =  go.GraphObject.make;
-    Diagram1 = $(go.Diagram, "DiagramDiv1")
-    var nodeData = [
+    document.getElementById("ModDiagrams").style.height = screen.height;
+    let $ =  go.GraphObject.make;
+    Diagram1 = $(go.Diagram, "ModDiagram1")
+    let nodeData = [
         {key : 1},
         {key : 2}
     ];
-    var linkData = [
-        {to: 2 , from: 1}
+    let linkData = [
+        {to: 2 , from: 1},
+        {to: 5 , from: 1}
     ];
     Diagram1.model = new go.GraphLinksModel(nodeData, linkData);
 
-    Diagram2 = $(go.Diagram, "DiagramDiv2")
+    Diagram2 = $(go.Diagram, "ModDiagram2")
     Diagram2.model = new go.GraphLinksModel(nodeData, linkData);
-    Diagram3 = $(go.Diagram, "DiagramDiv3")
+    Diagram3 = $(go.Diagram, "ModDiagram3")
     Diagram3.model = new go.GraphLinksModel(nodeData, linkData);
+    DiagramResult = $(go.Diagram, "DiagramResult")
+    DiagramResult.model = new go.GraphLinksModel(nodeData, linkData);
 }
+
